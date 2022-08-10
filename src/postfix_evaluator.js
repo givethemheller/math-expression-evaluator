0c9f25e6136754b535bdd72dbf23508a933cc2be
Tom <tom@pickaxelabs.com>
2022-08-09 22:44:45 -0700

    remove the inifinity parse issue

--- src/postfix_evaluator.js
+++ src/postfix_evaluator.js
@@ -76,7 +76,7 @@ Mexp.prototype.postfixEval = function (UserDefined) {
 	if (stack.length > 1) {
 		throw new Mexp.Exception('Uncaught Syntax error')
 	}
-	return stack[0].value > 1000000000000000 ? 'Infinity' : parseFloat(stack[0].value.toFixed(15))
+	return parseFloat(stack[0].value.toFixed(15))
 }
 Mexp.eval = function (str, tokens, obj) {
 	if (typeof tokens === 'undefined') {
